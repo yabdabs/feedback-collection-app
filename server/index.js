d6c0c33e4784ff36fd390de00a0b97124fcd0b75
Abdul Khan <abdul.k.khan75@gmail.com>
2018-01-14 05:39:42 -0500

    test

--- server/index.js
+++ server/index.js
@@ -33,13 +33,14 @@ require('./routes/billingRoutes.js')(app)
 
 if(process.env.NODE_ENV === 'production'){
 	const path = require('path')
+	
 // (If we do not have express route handler for the request), This first checks to see if there is a file the request is looking for. Express will serve up production assets like our main.js file or main.css file.
   app.use(express.static(path.join(__dirname, '/../client/build')));
 
-	// //if we don't know what the route is, serve up the index.html file
-	// app.get('*', (req,res) => {
-	// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	// })
+	//if we don't know what the route is, serve up the index.html file
+	app.get('*', (req,res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	})
 }
 
 const PORT = process.env.PORT || 5000;
