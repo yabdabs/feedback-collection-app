42afb98bc7492b9745b774e0e0cd576e00775a02
Abdul Khan <abdul.k.khan75@gmail.com>
2018-01-14 18:27:02 -0500

    test

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ require('./routes/billingRoutes.js')(app)
 // app.use('/auth', require('./routes/authRoutes'))
 
 if(process.env.NODE_ENV === 'production'){
-	const path = require('path')
+	var path = require('path')
 
 	//rando message again	sdfjmdlkm
 // (If we do not have express route handler for the request), This first checks to see if there is a file the request is looking for. Express will serve up production assets like our main.js file or main.css file.
