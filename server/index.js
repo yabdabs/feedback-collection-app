f2b8e83abffe25bba6e7cd1e8e2167c24bdcdc4c
Abdul Khan <abdul.k.khan75@gmail.com>
2018-01-14 04:22:57 -0500

    added billing and client side app. edit p.json for heroku build

--- server/index.js
+++ server/index.js
@@ -7,12 +7,11 @@ const bodyParser = require('body-parser')
 require('./services/passport')
 
 const app = express();
-
+const path = require('path')
 
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: false }))
 
-// 
 
 /*tell passport to make use of cookies to manage authentication inside of our application. The purpose of cookies is to help us keep track of the user who is logged in, since http is stateless. So whenever browser makes any request to the server, it will send that cookie(with identifying info) in the request. cookieSession extracts cookie data (and stores it in req.session)and then passport pulls user id out of cookie data, then deserializeUser.
 
@@ -31,15 +30,12 @@ app.use(passport.session())
 //Express app Routing 
 require('./routes/authRoutes.js')(app)
 require('./routes/billingRoutes.js')(app)
-
 //we would use the below if we were using express Router
 // app.use('/auth', require('./routes/authRoutes'))
 
 if(process.env.NODE_ENV === 'production'){
-	const path = require('path')
-
 // (If we do not have express route handler for the request), This first checks to see if there is a file the request is looking for. Express will serve up production assets like our main.js file or main.css file.
-	app.use(express.static(path.join(__dirname, '/../../client/build')))
+	app.use(express.static(path.join(__dirname, '/../client/build')))
 
 	// const path = require('path')
 	// //if we don't know what the route is, serve up the index.html file
