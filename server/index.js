2655110adce5b60d30dffb03a094201b14dc96fc
Abdul Khan <abdul.k.khan75@gmail.com>
2018-01-14 05:49:55 -0500

    random message

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ if(process.env.NODE_ENV === 'production'){
 
 	//rando message
 // (If we do not have express route handler for the request), This first checks to see if there is a file the request is looking for. Express will serve up production assets like our main.js file or main.css file.
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static('client/build'));
 
 	//if we don't know what the route is, serve up the index.html file
 	app.get('*', (req,res) => {
