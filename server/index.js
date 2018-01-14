c9439d14a88221687ec7da02b895b8078ff67b7a
Abdul Khan <abdul.k.khan75@gmail.com>
2018-01-14 03:23:42 -0500

    added billing and client side app. edit p.json for heroku build

--- server/index.js
+++ server/index.js
@@ -36,6 +36,17 @@ require('./routes/billingRoutes.js')(app)
 // app.use('/auth', require('./routes/authRoutes'))
 
 
+if(process.env.NODE_ENV === 'production'){
+// (If we do not have express route handler for the request), This first checks to see if there is a file the request is looking for. Express will serve up production assets like our main.js file or main.css file.
+	app.use(express.static('client/build'))
+
+	const path = require('path')
+	//if we don't know what the route is, serve up the index.html file
+	app.get('*', (req,res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
 	console.log(`listening on port ${PORT}`);
